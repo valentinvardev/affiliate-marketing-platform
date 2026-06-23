d226ee6f0b462cdeaf8eb7f621cd28f6c090dcad
Valentin <gatucarpinlila@gmail.com>
2026-06-23 20:45:17 -0300

    fix: restore next/image import in admin page for logo preset thumbnails

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/server/auth";
